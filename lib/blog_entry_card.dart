723b8dff6baf4d851d5f520a43d5029f61e4fa3e
Andrew Byrne <pachakutech@gmail.com>
2025-08-03 17:04:01 -0700

    blog entry card fixes

--- lib/blog_entry_card.dart
+++ lib/blog_entry_card.dart
@@ -11,130 +11,127 @@ class BlogEntryCard extends StatelessWidget {
   const BlogEntryCard({Key? key, required this.blogEntry}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, constraints) {
-      return Card(
-        margin: const EdgeInsets.all(4.0),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: StaggeredGrid.count(
-              crossAxisCount: constraints.maxWidth < 600 ? 2 : 4,
-              mainAxisSpacing: 4,
-              crossAxisSpacing: 4,
-              children: [
-                ...blogEntry?.content
-                        .map((block) => switch (block) {
-                              // Case: has title, link and image: link titled image
-                              BlogEntry_ContentBlock(
-                                title: final title,
-                                imageUrl: final imgUrl,
-                                linkUrl: final linkUrl
-                              )
-                                  when block.hasTitle() &&
-                                      title.isNotEmpty &&
-                                      block.hasImageUrl() &&
-                                      imgUrl.isNotEmpty &&
-                                      block.hasLinkUrl() &&
-                                      linkUrl.isNotEmpty =>
-                                StaggeredGridTile.count(
-                                    crossAxisCellCount: 2,
-                                    mainAxisCellCount: 3,
-                                    child: InkWell(
-                                      child: Column(
-                                        children: [
-                                          Image.network(imgUrl),
-                                        ],
-                                      ),
-                                      onTap: () =>
-                                          launchUrl(Uri.parse(linkUrl)),
-                                    )),
-                              // Case: has image and title
-                              BlogEntry_ContentBlock(
-                                imageUrl: final imgUrl,
-                                title: final title
-                              )
-                                  when block.hasImageUrl() &&
-                                      imgUrl.isNotEmpty &&
-                                      block.hasTitle() &&
-                                      title.isNotEmpty =>
-                                StaggeredGridTile.count(
-                                    crossAxisCellCount: 1,
-                                    mainAxisCellCount: 3,
-                                    child: Column(
-                                      children: [
-                                        Text(title),
-                                        Image.network(imgUrl)
-                                      ],
-                                    )),
-                              // Case: has title and link, no image: hyperlink or media link
-                              BlogEntry_ContentBlock(
-                                title: final title,
-                                linkUrl: final linkUrl
-                              )
-                                  when block.hasTitle() &&
-                                      title.isNotEmpty &&
-                                      block.hasLinkUrl() &&
-                                      linkUrl.isNotEmpty =>
-                                _wrapVideoLink(extractVideoId(linkUrl)) ??
-                                    _wrapHyperlink(title, linkUrl),
-                              // Case: has image and link, no title (link image)
-                              BlogEntry_ContentBlock(
-                                imageUrl: final imgUrl,
-                                linkUrl: final linkUrl
-                              )
-                                  when block.hasImageUrl() &&
-                                      imgUrl.isNotEmpty &&
-                                      block.hasLinkUrl() &&
-                                      linkUrl.isNotEmpty =>
-                                StaggeredGridTile.count(
-                                    crossAxisCellCount: 2,
-                                    mainAxisCellCount: 2,
-                                    child: _hyperlinkedImage(imgUrl, linkUrl)),
-                              // Case: image only
-                              BlogEntry_ContentBlock(imageUrl: final imgUrl)
-                                  when block.hasImageUrl() &&
-                                      imgUrl.isNotEmpty =>
-                                StaggeredGridTile.count(
-                                  crossAxisCellCount: 2,
-                                  mainAxisCellCount: 2,
-                                  child: Image.network(imgUrl),
-                                ),
-                              // Case: title (text) only
-                              BlogEntry_ContentBlock(title: final title)
-                                  when block.hasTitle() && title.isNotEmpty =>
-                                StaggeredGridTile.fit(
-                                  crossAxisCellCount: 1,
-                                  child: Text('title ONLY: $title'),
-                                ),
-                              // Default case
-                              _ => StaggeredGridTile.count(
-                                  crossAxisCellCount: 1,
-                                  mainAxisCellCount: 1,
-                                  child: Text('Unknown block type')),
-                            })
-                        .toList() ??
-                    []
-              ]),
-        ),
-      );
-    });
-  }
+  Widget build(BuildContext context) => LayoutBuilder(
+      builder: (context, constraints) => Card(
+            margin: const EdgeInsets.all(4.0),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: StaggeredGrid.count(
+                  crossAxisCount: constraints.maxWidth < 600 ? 2 : 4,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  children: [
+                    ...blogEntry?.content
+                            .map((block) => switch (block) {
+                                  // Case: has title, link and image: link titled image
+                                  BlogEntry_ContentBlock(
+                                    title: final title,
+                                    imageUrl: final imgUrl,
+                                    linkUrl: final linkUrl
+                                  )
+                                      when block.hasTitle() &&
+                                          title.isNotEmpty &&
+                                          block.hasImageUrl() &&
+                                          imgUrl.isNotEmpty &&
+                                          block.hasLinkUrl() &&
+                                          linkUrl.isNotEmpty =>
+                                    StaggeredGridTile.count(
+                                        crossAxisCellCount: 2,
+                                        mainAxisCellCount: 3,
+                                        child: InkWell(
+                                          child: Column(
+                                            children: [
+                                              Image.network(imgUrl),
+                                            ],
+                                          ),
+                                          onTap: () =>
+                                              launchUrl(Uri.parse(linkUrl)),
+                                        )),
+                                  // Case: has image and title
+                                  BlogEntry_ContentBlock(
+                                    imageUrl: final imgUrl,
+                                    title: final title
+                                  )
+                                      when block.hasImageUrl() &&
+                                          imgUrl.isNotEmpty &&
+                                          block.hasTitle() &&
+                                          title.isNotEmpty =>
+                                    StaggeredGridTile.count(
+                                        crossAxisCellCount: 1,
+                                        mainAxisCellCount: 3,
+                                        child: Column(
+                                          children: [
+                                            Text(title),
+                                            Image.network(imgUrl,
+                                                errorBuilder:
+                                                    (cx, err, stack) =>
+                                                        Text(err.toString())),
+                                          ],
+                                        )),
+                                  // Case: has title and link, no image: hyperlink or media link
+                                  BlogEntry_ContentBlock(
+                                    title: final title,
+                                    linkUrl: final linkUrl
+                                  )
+                                      when block.hasTitle() &&
+                                          title.isNotEmpty &&
+                                          block.hasLinkUrl() &&
+                                          linkUrl.isNotEmpty =>
+                                    _wrapVideoLink(title, extractVideoId(linkUrl)) ??
+                                        _wrapHyperlink(title, linkUrl),
+                                  // Case: has image and link, no title (link image)
+                                  BlogEntry_ContentBlock(
+                                    imageUrl: final imgUrl,
+                                    linkUrl: final linkUrl
+                                  )
+                                      when block.hasImageUrl() &&
+                                          imgUrl.isNotEmpty &&
+                                          block.hasLinkUrl() &&
+                                          linkUrl.isNotEmpty =>
+                                    StaggeredGridTile.count(
+                                        crossAxisCellCount: 2,
+                                        mainAxisCellCount: 2,
+                                        child:
+                                            _hyperlinkedImage(imgUrl, linkUrl)),
+                                  // Case: image only
+                                  BlogEntry_ContentBlock(imageUrl: final imgUrl)
+                                      when block.hasImageUrl() &&
+                                          imgUrl.isNotEmpty =>
+                                    StaggeredGridTile.count(
+                                      crossAxisCellCount: 2,
+                                      mainAxisCellCount: 2,
+                                      child: Image.network(imgUrl),
+                                    ),
+                                  // Case: title (text) only
+                                  BlogEntry_ContentBlock(title: final title)
+                                      when block.hasTitle() &&
+                                          title.isNotEmpty =>
+                                    StaggeredGridTile.fit(
+                                      crossAxisCellCount: 1,
+                                      child: Text('title ONLY: $title'),
+                                    ),
+                                  // Default case
+                                  _ => StaggeredGridTile.count(
+                                      crossAxisCellCount: 1,
+                                      mainAxisCellCount: 1,
+                                      child: Text('Unknown block type')),
+                                })
+                            .toList() ??
+                        []
+                  ]),
+            ),
+          ));
 
   StaggeredGridTile _wrapHyperlink(String title, String linkUrl) =>
       StaggeredGridTile.count(
         crossAxisCellCount: 2,
         mainAxisCellCount: 1,
-        child: Column(
-          children: [
-            _addHyperlink(
-                title.split('/'), () => launchUrl(Uri.parse(linkUrl))),
-            Image.network(linkUrl)
-          ],
-        ),
+        child: _addHyperlink(
+            title.split('/'), () => launchUrl(Uri.parse(linkUrl))),
       );
 }
 
-_wrapVideoLink(extractVideoId) => extractVideoId == null
+_wrapVideoLink(title, extractVideoId) => extractVideoId == null
     ? null
     : StaggeredGridTile.count(
         crossAxisCellCount: 2,
